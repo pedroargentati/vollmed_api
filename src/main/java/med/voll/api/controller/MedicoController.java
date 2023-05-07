638c9239de928dd08297b669996e7d2798e58f85
Pedro Argentati <pedro.argentatii@gmail.com>
2023-05-07 11:37:53 -0300

    padronizando endpoints medio

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosAlteracaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosDetalhamentoMedio;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -30,35 +33,49 @@ public class MedicoController {
 	private MedicoRepository medicoRepository;
 	
 	@GetMapping
-	public Page<DadosListagemMedico> obterListaMedicos(@PageableDefault(size = 15, sort = {"nome"}) Pageable paginacao /**Serve para realizar a paginação*/) {
-		return medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+	public ResponseEntity<Page<DadosListagemMedico>> obterListaMedicos(@PageableDefault(size = 15, sort = {"nome"}) Pageable paginacao /**Serve para realizar a paginação*/) {
+		var page =  medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+		return ResponseEntity.ok(page);
 	}
 	
 	@PostMapping
 	@ResponseBody
 	@Transactional
-	public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
+	public ResponseEntity cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
 		/**
 		 * No controller não recebemos um objeto do tipo médico, e sim um DTO (DadosCadastoMedico)
 		 * então será necessário fazer uma conversão de DTO para um objeto do tipo Medico (JPA Entity).
 		 */
-		medicoRepository.save(new Medico(dados));
+		var medico = new Medico(dados);
+		medicoRepository.save(medico);
+		
+		/** Precisamos devolver aonde o recurso foi criado. Para isso, usamos a classe do Spring  'UriComponentsBuilder'
+		 *  e junto com ela o ID do médico que foi criado.
+		 */
+		var uri = uriBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+		                      // 201
+		return ResponseEntity.created(uri).body(new DadosDetalhamentoMedio(medico));
 	}
 	
 	@PutMapping
 	@ResponseBody
 	@Transactional
-	public void atulizarMedico(@RequestBody @Valid DadosAlteracaoMedico dados) {
+	public ResponseEntity<DadosDetalhamentoMedio> atulizarMedico(@RequestBody @Valid DadosAlteracaoMedico dados) {
 		var medico = medicoRepository.getReferenceById(dados.id());
 		medico.atualizarInformacoes(dados);
+		return ResponseEntity.ok(new DadosDetalhamentoMedio(medico));
 	}
+	
 	
 	@DeleteMapping("/{id}")
 	@ResponseBody
 	@Transactional
-	public void excluirMedico(@PathVariable("id") Long id) {
+	public ResponseEntity<?> excluirMedico(@PathVariable("id") Long id) {
 		var medico = medicoRepository.getReferenceById(id);
 		medico.excluir();
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
