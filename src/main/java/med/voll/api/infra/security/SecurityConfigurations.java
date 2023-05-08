e7e01469a7b26caaa105fd0d77fe95dc47d57dc0
Pedro Argentati <pedro.argentatii@gmail.com>
2023-05-07 21:37:36 -0300

    Controller de autenticação

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +25,25 @@ public class SecurityConfigurations {
 		return http.csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // muda a configuração para stateless ao invés de statefull.
 				.and().build();
+	}
+	
+	/**
+	 * @description Ensina ao spring como injetar o objeto do tipo AuthenticationManager.
+	 * 
+	 * @param configuration
+	 * @throws Exception
+	 */
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
+	
+	@Bean
+	/**
+	 * @description Ensina ao spring que deverá usar o algoritmo 'BCryptPasswordEncoder'.
+	 */
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
