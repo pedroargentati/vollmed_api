138ae9f333fee38f856f8dee7f75f22ecb86e664
Pedro Argentati <pedro.argentatii@gmail.com>
2023-05-20 22:40:03 -0300

    feat: implementando geração de token via JWT.

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,49 @@
+package med.voll.api.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import med.voll.api.domain.usuario.Usuario;
+
+@Service
+public class TokenService {
+
+	/**
+	 * Utilizando a biblioteca JWT, gera o token para a aplicação.
+	 *
+	 * @param Usuario dados do usuário.
+	 * @return token
+	 */
+	public String gerarToken(Usuario usuario) {
+		try {
+			var algoritmo = Algorithm.HMAC256("12345678");
+			return JWT.create()
+					.withIssuer("API Voll.med")
+					.withSubject(usuario.getLogin()) // usuário relacionado com este token.
+					.withExpiresAt(dataExpiracao()) // validade do token.
+					.sign(algoritmo);
+		} catch (JWTCreationException exception) {
+			throw new RuntimeException("[ERRO] Erro ao gerar o Token JWT.", exception);
+		}
+	}
+
+	/**
+	 * Calcula a data de expiração do token adicionando 2 horas
+	 * ao horário atual do sistema, considerando o fuso horário GMT-3.
+	 *
+	 * @return Objeto Instant que representa a data e hora de expiração.
+	 */
+	private Instant dataExpiracao() {
+		return LocalDateTime.now()
+				.plusHours(2)
+				.toInstant(ZoneOffset.of("-03:00"));
+	}
+}
