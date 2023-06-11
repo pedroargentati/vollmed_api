dd2e28033ee8188d5f464c0a97c24060d886f552
Pedro <pedro.argentatii@gmail.com>
2023-06-11 19:13:18 -0300

    feat: validações e regras de agendamento

---
+++ src/main/java/med/voll/api/domain/BusinessException.java
@@ -0,0 +1,9 @@
+package med.voll.api.domain;
+
+public class BusinessException extends RuntimeException {
+
+	public BusinessException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -3,6 +3,8 @@ package med.voll.api.domain.consulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import med.voll.api.domain.BusinessException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 
@@ -19,12 +21,34 @@ public class AgendaDeConsultas {
 	private PacienteRepository pacienteRepository;
 	
 	public void agendar(DadosAgendamentoConsulta dados) {
+		if (!pacienteRepository.existsById(dados.idPaciente())) {
+			throw new BusinessException("Id do paciente informado não existe.");
+		}
 		
-		var medico = medicoRepository.findById(dados.idMedico()).get();
-		var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+		if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+			throw new BusinessException("Id do médico informado não existe.");			
+		}
+		
+		var medico = escolherMedico(dados);
+		var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
 		var consulta = new Consulta(null, medico, paciente, dados.data());
 		
 		consultaRepository.save(consulta);
 	}
+
+	private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+		if (dados.idMedico() != null) {
+			return medicoRepository.getReferenceById(dados.idMedico());
+		}
+		
+		if (dados.especialidade() == null) {
+			throw new BusinessException("A especialidade é obrigatório quando médico não for escolhido!");
+		}
+		
+		return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+		
+	}
+	
+	
 	
 }
